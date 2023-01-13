09bcf3b12bfb7afa88716c4e770aeb66d898f5df
ymm14 <omniee@gmail.com>
2023-01-13 13:15:49 -0500

    adding sniffles2 and pbsv to svtk standardizer

--- src/svtk/svtk/standardize/__init__.py
+++ src/svtk/svtk/standardize/__init__.py
@@ -8,4 +8,3 @@ from .std_scramble import ScrambleStandardizer
 from .std_smoove import SmooveStandardizer
 from .std_pbsv import PBSVStandardizer
 from .std_sniffles import SnifflesStandardizer
-from .std_hgsvc import HGSVCStandardizer
